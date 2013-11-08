cba09af92c024b1af546750c7865047488af2609
Danielx64 <danielx386@gmail.com>
2013-11-08 16:18:23 +1100

    Added checking code so that people don't install this on an unsupported wordpress version. Work still needs to be done.

--- phpBB-WordPress-bridge/Bridge-phpBB-WordPress/wordpress/wp-content/themes/phpBB/functions.php
+++ phpBB-WordPress-bridge/Bridge-phpBB-WordPress/wordpress/wp-content/themes/phpBB/functions.php
@@ -13,6 +13,20 @@
 * @ignore
 **/
 
+function propress_validate_wordpress() {
+	// Don't validate during an install/upgrade.
+	if ( defined('WP_INSTALLING') || !apply_filters( 'validate_current_theme', true ) )
+		return true;
+if (!function_exists('wp_cache_switch_to_blog')){
+		switch_theme( WP_DEFAULT_THEME );
+			    wp_die( __('Propress requires WordPress 3.5.0 and higher. The default theme has been loaded.', 'propress') );
+		return false;
+	}
+	return true;
+}
+add_action( 'after_setup_theme', 'propress_validate_wordpress' );
+
+
 require( get_template_directory() . '/includes/options.php' );
 require( get_template_directory() . '/includes/custom.php' );
 
