d5832ab69afd47f97e44836bfac6a4fca4e8f856
Danielx64 <danielx386@gmail.com>
2013-11-08 19:23:53 +1100

    Remove more crap from the header

--- phpBB-WordPress-bridge/Bridge-phpBB-WordPress/wordpress/wp-content/themes/phpBB/functions.php
+++ phpBB-WordPress-bridge/Bridge-phpBB-WordPress/wordpress/wp-content/themes/phpBB/functions.php
@@ -51,8 +51,16 @@ function redirect_non_admin_users() {
 // Hide WordPress Admin Bar
 add_filter('show_admin_bar', '__return_false');
 
-// Remove wordpress version from header
+// remove junk from head
+remove_action('wp_head', 'rsd_link');
 remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'feed_links', 2);
+remove_action('wp_head', 'index_rel_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'feed_links_extra', 3);
+remove_action('wp_head', 'start_post_rel_link', 10, 0);
+remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
 
 // Add session id
 add_filter('logout_url', 'wp_phpbb_loginout', 1, 2);
