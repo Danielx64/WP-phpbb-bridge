1401bee36f43a654057e9817c89fac77eb2cd09e
Danielx64 <danielx386@gmail.com>
2014-02-22 18:45:29 +1100

    Fixed logn link

--- phpBB-WordPress-bridge/wordpress plugin/phpbb/phpbb-load.php
+++ phpBB-WordPress-bridge/wordpress plugin/phpbb/phpbb-load.php
@@ -21,6 +21,8 @@ if ( !function_exists( 'add_action' ) ) {
 include_once dirname( __FILE__ ) . '/functions/wp-crosspost.php';
 include_once dirname( __FILE__ ) . '/functions/options.php';
 include_once dirname( __FILE__ ) . '/functions/custom.php';
+include_once dirname( __FILE__ ) . '/functions/wp-profile.php';
+
 require( ABSPATH . WPINC . '/pluggable.php' );
 
 	if (!defined('IN_WP_PHPBB_BRIDGE'))
@@ -38,4 +40,66 @@ function wp_phpbb_logout()
 	$temp =  phpbb::$config['wp_phpbb_bridge_board_path'];
 	 return $temp.'ucp.php?mode=logout&amp;sid='.phpbb::$user->session_id;
 }
+
+add_filter( 'login_url', 'wp_phpbb_login' );
+function wp_phpbb_login()
+{
+	$temp =  phpbb::$config['wp_phpbb_bridge_board_path'];
+	 return $temp.'ucp.php?mode=login';
+}
+
+// Thank-you Dion Designs :)
+function show_phpbb_link($content) {
+	if (!defined('IN_WP_PHPBB_BRIDGE'))
+	{
+		global $wp_phpbb_bridge_config, $phpbb_root_path, $phpEx;
+		global $auth, $config, $db, $template, $user, $cache;
+		include(TEMPLATEPATH . '/includes/wp_phpbb_bridge.php');
+	}
+	$postID = get_the_ID();
+	if(empty($postID)) {
+		return $content;
+	}
+	$sql = 'SELECT topic_id, forum_id, topic_replies FROM ' . TOPICS_TABLE . ' WHERE topic_wp_xpost = ' . $postID;
+	$result = phpbb::$db->sql_query($sql);
+	$post_data = phpbb::$db->sql_fetchrow($result);
+	$board_url = generate_board_url(false) . '/';
+	$web_path = (defined('PHPBB_USE_BOARD_URL_PATH') && PHPBB_USE_BOARD_URL_PATH) ? $board_url : PHPBB_ROOT_PATH;
+	$replies = $post_data['topic_replies'];
+
+	if ($post_data) {
+		$rsuffix = $rtext = $rbutton = '';
+		if ($replies) {
+			if ($replies != 1) {
+				$rsuffix = 's';
+			}
+			$rbutton = '&nbsp;&nbsp;&nbsp;&nbsp;<a class="button1" href="' . $web_path . 'viewtopic.php?f='. $post_data['forum_id'] . '&amp;t=' . $post_data['topic_id'] . '">View the Discussion</a>';
+			$rtext =  '' . $replies . ' Comment' . $rsuffix;
+		}
+		$content .= '<div class="xpost-link">' . '<a class="button1" href="' . $web_path . 'posting.php?mode=reply&amp;f=' . $post_data['forum_id'] . '&amp;t=' . $post_data['topic_id'] . '">Comment On this Article</a>' . $rbutton . '</div>' . $rtext;
+	}
+	return $content;
+}
+add_filter('the_content', 'show_phpbb_link');
+
+
+// Don't nag users who can't switch themes.
+if ( ! is_admin() || ! current_user_can( 'switch_themes' ) )
+	return;
+
+function wphpbb_admin_notice() {
+	if ( isset( $_GET['wphpbb-dismiss'] ) )
+		set_theme_mod( 'wphpbb', true );
+
+	$dismiss = get_theme_mod( 'wphpbb', false );
+	if ( $dismiss )
+		return;
+	?>
+	<div class="updated wphpbb-notice">
+		<p><?php printf( __( 'In order for this bridge to work correctly, you will <a target="_blank" href="%s">need to configure it</a>. <a href="%s">I have already configured it.</a>', 'wp_phpbb3_bridge' ), admin_url('themes.php?page=propress-settings'), add_query_arg( 'wphpbb-dismiss', 1 ) ); ?></p>
+	</div>
+	<?php
+}
+add_action( 'admin_notices', 'wphpbb_admin_notice' );
+
 ?>
