06a6dd66309b8fd4c6c44425a0490d96537a0d5c
Danielx64 <danielx386@gmail.com>
2014-05-17 16:39:53 +1000

    wasn't quite right now it much better

--- phpBB-WordPress-bridge/Bridge-phpBB-WordPress/wordpress/wp-content/themes/phpBB/settings/plugin-main.php
+++ phpBB-WordPress-bridge/Bridge-phpBB-WordPress/wordpress/wp-content/themes/phpBB/settings/plugin-main.php
@@ -5,211 +5,115 @@
 * @copyright (c) 2013-2014 danielx64.com
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License  
 * @author Danielx64
-* 
-* @based off WP-United
-* @orginal author John Wells wp-united.com
+ * 
+ */
+/*
+Plugin Name: phpBB to WP connector (plugin version)
+Plugin URI: http://wordpress.org/plugins/hello-dolly/
+Description: WordPress - phpBB Integration Mod This Birdge makes possible to integrate your phpBB into your Wordpress Blog, sharing users. If the phpBB users do not exist in WP it will be automatically created as a "Subscriber" Want to have wordpress match your forum style? Get the theme instead.
+Author: Danielx64
+Version: 1.5
+Author URI: http://danielx64.com/
 */
 
-/** 
-	Much of this was taken from wp-united and is only used to create the option page.
-	While it is not ideal, that's all I got for now.
- */
-
-class WP_United_Plugin extends WP_United_Plugin_Main_Base {
-
-	protected
-		// Actions and filters. These are loaded as needed depending on which WP-United portions are active.
-		// Format: array( event | function in this class(in an array if optional arguments are needed) | loading circumstances)
-		$actions = array(
-			// required actions on all pages
-			array('plugins_loaded', 					'init_plugin',								'all'),  // this should be 'init', but we want to play with current_user, which comes earlier
-
-			// required admin ajax actions
-			array('wp_ajax_wpu_filetree', 				'filetree',									'all'),
-
-			array('wp_ajax_wpu_settings_transmit', 		'ajax_settings_transmit',					'all'),
-
-		);
-
-		
-		private
-			$doneInit 		= false;
-
-	/**
-	* All base init is done by the parent class.
-	*/
-	public function __construct() {
-
-		parent::__construct();
-		
-	}
-	
-	/**
-	 * Initialises the plugin from WordPress.
-	 * This is not in the constructor, as this class can be instantiated from either phpBB or WordPress.
-	 * @return void
-	 */
-	public function wp_init() {
-	
-		// (re)load our settings
-		$this->load_settings();
-
-		// add new actions and filters
-		$this->add_actions();
-
-		unset($this->actions);
-
-	}
-	
-	/**
-	 * The main invocation logic -- if enabled, load phpBB too!
-	 * Called on plugins_loaded hook, so we can get phpBB ready in advance of user integration when set_current_user is called.
-	 * @return void
-	 */
-	public function init_plugin() { 
-		global $phpbbForum;
-
-		if($this->has_inited()) {
-			return false;
-		}
-		$this->doneInit = true;
-		
-		$shouldRun = true;
-
-		$propress_options = get_option( 'phpbbtowp' );
-		if(!$propress_options['phpbb_path'] || !WP_United_Plugin::can_connect_to_phpbb()) {
-			$this->set_last_run('disconnected');
-			$shouldRun = false;
-		}
-
-
-		if($this->get_last_run() == 'connected') {
-			$shouldRun = false;
-		}
-		
-		if($shouldRun) {
-			$this->set_last_run('connected');
-		}
-		
-		$versionCheck = $this->check_mod_version();
-		if($versionCheck['result'] != 'OK') {
-			$this->disable();
-			$shouldRun = false;
-		}
-		
-		if($this->is_enabled() && $shouldRun) { 
-
-			$this->set_last_run('working');
-
-		}
-
-		return true; 
-			
-	}
-
-	public function can_connect_to_phpbb() {
-		global $wpUnited;
-		$propress_options = get_option( 'phpbbtowp' );
-		$rootPath = $propress_options['phpbb_path'];
-
-		if(!$rootPath) {
-			return false;
-		}
-
-		static $canConnect = false;
-		static $triedToConnect = false;
-
-		if($triedToConnect) {
-			return $canConnect;
-		}
-
-		$canConnect = @file_exists($rootPath);
-		$triedToConnect = true;
-
-
-		return $canConnect;
-
-	}
-	/**
-	 * 
-	 * Admin AJAX actions
-	 *
-	*/
-	public function filetree() {
-		if(check_ajax_referer( 'wp-united-filetree')) {
-			wpu_filetree();
-		}
-		die();
-	}
-
-	public function ajax_settings_transmit() {
-		if(check_ajax_referer( 'wp-united-transmit')) {
-			wpu_process_settings();
-			$this->transmit_settings();
-			die('OK');
-		}
-		die();
-	}	
-
-	/**
-	 * Transmit settings to phpBB
-	 * This could either be an update settings or enable rquest, or a disable request.
-	 * @param bool $enable true to enable WP-United in phpBB config
-	 * @return void
-	 */
-	public function transmit_settings($enable = true) {
-		global $phpbbForum;
-		
-		//if WPU was disabled, we need to initialise phpBB first
-		// phpbbForum is already inited, however -- we just need to load
-		if (!defined('IN_PHPBB')) {
-			
-			$this->set_last_run('connected');
-
-		}
-	
-		// store data before transmitting
-		if($enable) {
-			$this->enable();
-		} else {
-			$this->disable();
-		}
-		
-		$dataToStore = $this->settings;
-	}
-
-	/**
-	 * Returns true if WP-United has already initialised
-	 * @return bool true if already inited
-	 */
-	public function has_inited() {
-		return $this->doneInit;
-	}
-	
-	/**
-	 * A way of storing how the last run of phpBB went -- only used during connecting and enabling phpBB
-	 * States transition through disconnected -> connected -> working
-	 * @param string $status disconnected|connected|working
-	 */
-	private function set_last_run($status) {
-		if($this->get_last_run() != $status) { 
-			// transitions cannot go from 'working' to 'connected' if wp-united is enabled OK.
-			if( ($this->lastRun == 'working') && ($status == 'connected') && $this->is_enabled() ) {
-				return;
-			} 
-			$this->lastRun = $status;
-			update_option('wpu-last-run', $status);
-		}
-	}
-	
-	/**
-	 * Updates the stored WP-United settings
-	 * The Wp-United class decorates itself with the stored phpBB or WordPress settings class, invoked as appropriate. 
-	 * WP settings take priority
-	 * @param array an array of all settings keys
-	 * @return void
-	 */
-	public function update_settings($data) {
-		$this->settings->update_settings($data);
-	}
+// Make sure we don't expose any info if called directly
+if (!function_exists('add_action')) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
 }
+
+include_once dirname( __FILE__ ) . '/functions/wp-crosspost.php';
+add_action( 'publish_post', 'wp_phpbb_posting', 10, 2);
+include_once dirname( __FILE__ ) . '/functions/options.php';
+include_once dirname( __FILE__ ) . '/functions/wp-profile.php';
+if( !class_exists( 'WP_United_Plugin' ) ) {
+	require_once(dirname( __FILE__ ) . '/functions/base-classes.php');
+	require_once(dirname( __FILE__ ) . '/functions/plugin-main.php');
+	global $wpUnited;
+	$wpUnited = new WP_United_Plugin();
+}
+$wpUnited->wp_init();
+require( ABSPATH . WPINC . '/pluggable.php' );
+global $pagenow;
+	if (!preg_match('/\/wp-admin\/(themes.php|plugins.php|admin-ajax.php)/', $_SERVER['REQUEST_URI'])) {
+			include_once dirname(__FILE__) . '/functions/wp_phpbb_bridge.php';
+			add_filter('login_url', 'wp_phpbb_login');
+			add_filter('logout_url', 'wp_phpbb_logout');
+			add_filter('register_url', 'wp_phpbb_register');
+	}
+
+
+function wp_phpbb_logout()
+{
+	$temp = generate_board_url() . '/';
+	return $temp . 'ucp.php?mode=logout&amp;sid=' . phpbb::$user->session_id;
+}
+
+function wp_phpbb_register()
+{
+	$temp = generate_board_url() . '/';
+	return $temp . 'ucp.php?mode=register';
+}
+
+function wp_phpbb_login()
+{
+	$redirect = request_var('redirect', home_url(add_query_arg(array())));
+	$temp = generate_board_url() . '/';
+	return $temp . 'ucp.php?mode=login&amp;redirect=' . $redirect;
+}
+
+// Thank-you Dion Designs :)
+function show_phpbb_link($content)
+{
+	if (!defined('IN_WP_PHPBB_BRIDGE')) {
+		global $wp_phpbb_bridge_config, $phpbb_root_path, $phpEx;
+		global $auth, $config, $db, $template, $user, $cache;
+		include( dirname( __FILE__ ) . '/functions/wp_phpbb_bridge.php');
+	}
+	$postID = get_the_ID();
+	if (empty($postID)) {
+		return $content;
+	}
+	$sql = 'SELECT topic_id, forum_id, topic_replies FROM ' . TOPICS_TABLE . ' WHERE topic_wp_xpost = ' . $postID;
+	$result = phpbb::$db->sql_query($sql);
+	$post_data = phpbb::$db->sql_fetchrow($result);
+	$board_url = generate_board_url(false) . '/';
+	$web_path = (defined('PHPBB_USE_BOARD_URL_PATH') && PHPBB_USE_BOARD_URL_PATH) ? $board_url : PHPBB_ROOT_PATH;
+	$replies = $post_data['topic_replies'];
+
+	if ($post_data) {
+		$rsuffix = $rtext = $rbutton = '';
+		if ($replies) {
+			if ($replies != 1) {
+				$rsuffix = 's';
+			}
+			$rbutton = '&nbsp;&nbsp;&nbsp;&nbsp;<a class="button1" href="' . $web_path . 'viewtopic.php?f=' . $post_data['forum_id'] . '&amp;t=' . $post_data['topic_id'] . '">View the Discussion</a>';
+			$rtext = '' . $replies . ' Comment' . $rsuffix;
+		}
+		$content .= '<div class="xpost-link">' . '<a class="button1" href="' . $web_path . 'posting.php?mode=reply&amp;f=' . $post_data['forum_id'] . '&amp;t=' . $post_data['topic_id'] . '">Comment On this Article</a>' . $rbutton . '</div>' . $rtext;
+	}
+	return $content;
+}
+add_filter('the_content', 'show_phpbb_link');
+
+// Don't nag users who can't switch themes.
+if ( ! is_admin() || ! current_user_can( 'switch_themes' ) )
+return;
+
+function wphpbb_admin_notice()
+{
+	if (isset($_GET['wphpbb-dismiss']))
+		set_theme_mod('wphpbb', true);
+
+	$dismiss = get_theme_mod('wphpbb', false);
+	if ($dismiss)
+		return;
+	?>
+	<div class="updated wphpbb-notice">
+		<p><?php printf(__('In order for this bridge to work correctly, you will <a target="_blank" href="%s">need to configure it</a>. <a href="%s">I have already configured it.</a>', 'wp_phpbb3_bridge'), admin_url('themes.php?page=propress-settings'), add_query_arg('wphpbb-dismiss', 1)); ?></p>
+	</div>
+<?php
+}
+add_action( 'admin_notices', 'wphpbb_admin_notice' );
+
+?>
